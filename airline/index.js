d886aae97a6b1b56bb877bba80cfc682d9c6344f
Ilya <barykoilyass@gmail.com>
2021-01-29 22:00:05 +0300

    Can add records

--- airline/index.js
+++ airline/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Flight = require('./Model/flight')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const upload = multer({dest: "uploads/"})
 const app = express()
 const path = require('path')
 const port = 3000
-const mongoose = require('mongoose');
-const Flight = require('./Model/flight')
 
 const dbURI = 'mongodb+srv://admin:admin@mpp.zysk6.mongodb.net/mpp-lab?retryWrites=true&w=majority'
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -11,37 +14,39 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .catch((error) => console.log(error))
 
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(upload.array());
+
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
+const render = async(res) => {
     Flight.find()
         .then((result) => {
-            res.send(result)
+            res.render("home", {
+                flights: result
+            })
         })
         .catch((err) => {
             res.send(err)
         })
+}
+
+app.get('/', async (req, res) => {
+    await render(res)
 })
 
-app.post('/', function (req, res) {
-    res.send('Got a POST request')
-    // const flight = new Flight({
-    //     destination: "Moscow",
-    //     date: "2Sep",
-    //     file: 'nil',
-    //     price: 100
-    // })
-    // flight.save()
-    //     .then((result) => console.log(result))
-    //     .catch((err) => console.log(err))
-})
+app.post('/', upload.single("file"), async (req, res) => {
+    const { destination, date, price } = req.body
+    const { file } = req
 
-app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user');
-})
-
-app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user');
+    const flight = new Flight({
+        destination: destination,
+        date: date,
+        file: file.path,
+        price: price
+    })
+    await flight.save()
+    await render(res)
 })
 
 app.listen(port, () => {
